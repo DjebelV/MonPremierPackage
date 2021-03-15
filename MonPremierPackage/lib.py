019936acb9d7a2caeb0be6f61b8e5fec4585c319
Jerome Valette <valette.jerome@gmail.com>
2021-03-15 13:07:09 +0100

    stuff

---
+++ MonPremierPackage/lib.py
@@ -0,0 +1,9 @@
+def try_me():
+    print("Hello, can you go slap Ghislain for me please ?")
+    print("Can you also play this please ? -> https://open.spotify.com/track/2ZJ28Rm7OXMYJshLtp5uff?si=08WniGlwSZy8Swnywj5E2Q thanks")
+    a='Well done'
+    return 
+
+if __name__ == "__main__":  
+    a=try_me()
+    print(a)
